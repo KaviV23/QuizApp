b5cd13633dbf8aa63717430874736fe7085f28ea
Kaviraj Vijayanthiran <023kavi@gmail.com>
2024-06-22 13:09:45 +0800

    Added ResultActivity and respective layout

--- app/src/main/java/com/csc2074/assignment1/geoquiz/QuizQuestionsActivity.kt
+++ app/src/main/java/com/csc2074/assignment1/geoquiz/QuizQuestionsActivity.kt
@@ -20,6 +20,7 @@ import androidx.core.view.WindowInsetsCompat
 private var mCurrentPosition: Int = 1
 private var mQuestionsList: ArrayList<Question>? = null
 private var mSelectedOptionPosition: Int = 0
+private var mCorrectAnswers: Int = 0
 
 
 class QuizQuestionsActivity : AppCompatActivity(), View.OnClickListener{
@@ -149,6 +150,8 @@ class QuizQuestionsActivity : AppCompatActivity(), View.OnClickListener{
                     val question = mQuestionsList?.get(mCurrentPosition -1)
                     if(question!!.correctAnswer != mSelectedOptionPosition) {
                         answerView(mSelectedOptionPosition, R.drawable.wrong_option_border_bg)
+                    } else {
+                        mCorrectAnswers++
                     }
                     answerView(question.correctAnswer, R.drawable.correct_option_border_bg)
 
